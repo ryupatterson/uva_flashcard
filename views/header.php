781ec6e264d05ab88536fd32a46624080cba73cd
ryupatterson <54785861+ryupatterson@users.noreply.github.com>
2022-04-15 20:19:41 -0400

    ok

--- views/header.php
+++ views/header.php
@@ -7,13 +7,13 @@
         <p>Welcome, <?=$_SESSION["username"]?></p>
       </div>
       <div class="col-2">
-        <a href="<?=$this->base_url?>/account/my_decks"><p>My Decks</p></a>
+        <a href="<?=$this->base_url?>/account/my_decks/"><p>My Decks</p></a>
       </div>
       <div class="col-2">
-        <a href="<?=$this->base_url?>/deck/redirect"><p>Create Deck</p></a>
+        <a href="<?=$this->base_url?>/deck/redirect/"><p>Create Deck</p></a>
       </div>
       <div class="col-2">
-        <a href="<?=$this->base_url?>/account/logout"><p>Logout</p></a>
+        <a href="<?=$this->base_url?>/account/logout/"><p>Logout</p></a>
       </div>
       <div class="col-3">
         <input type="text" name="search" value="">
